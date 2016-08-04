c0985469175463cc249e46ebf4b89aa8922954bc
Henry Acosta <acosta.henry@gmail.com>
2016-08-04 21:10:29 +0000

    travis-implementation-completed-but -not-working

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
+// exports.DATABASE_URL = process.env.DATABASE_URL ||
+//                       global.DATABASE_URL ||
+//                       (process.env.NODE_ENV === 'production' ?
+//                             'mongodb://localhost/shopping-list' :
+//                             'mongodb://localhost/shopping-list-dev');
+// exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://Henry1425:jailyn1425@ds139645.mlab.com:39645/shopping-list-travis' :
+                            'mongodb://Henry1425:jailyn1425@ds139645.mlab.com:39645/shopping-list-travis');
 exports.PORT = process.env.PORT || 8080;
